b74e773a7b9ff25844b8ee0e70de499005224584
nlypage <qushedo@gmail.com>
2025-02-01 21:46:41 +0300

    remove: unused error

--- errors.go
+++ errors.go
@@ -3,6 +3,5 @@ package intele
 import "errors"
 
 var (
-	ErrTimeout           = errors.New("input timeout")
-	ErrTooManyConcurrent = errors.New("too many concurrent requests")
+	ErrTimeout = errors.New("input timeout")
 )
